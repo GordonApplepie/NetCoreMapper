a280dd9eabd7a0d9e45e5e346f86add35ee189fb
Gordon Mundt <gordon.mundt@live.com>
2017-03-17 09:37:25 -0500

    added data annotations, updated version to 1.0.2

---
+++ Annotations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GordonApplepie.NetCoreMapper.Annotations
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false)]
+    public class NoMapAttribute : System.Attribute
+    {
+        private List<Type> _types;
+
+        public NoMapAttribute()
+        {
+        }
+
+        public NoMapAttribute(params Type[] types)
+        {
+            _types = types.ToList();
+        }
+
+        public bool DoMap (Type type)
+        {
+            if (_types == null || _types.Count == 0 || _types.Contains(type))
+                return false;
+            else
+                return true;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, Inherited = false)]
+    public class OnlyMapAttribute : System.Attribute
+    {
+        private List<Type> _types;
+
+        public OnlyMapAttribute(params Type[] types)
+        {
+            _types = types.ToList();
+        }
+
+        public bool DoMap(Type type)
+        {
+            if (_types != null && _types.Contains(type))
+                return true;
+            else
+                return false;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, Inherited = false)]
+    public class MapOnAttribute : System.Attribute
+    {
+        private List<string> _properties = new List<string>();
+
+        public MapOnAttribute(params string[] properties)
+        {
+            _properties = properties.Select(x => x.ToLower()).ToList();
+        }
+
+        public virtual List<string> AllowedProperties
+        {
+            get
+            {
+                return _properties;
+            }
+        }
+    }
+}
--- CoreMapper.cs
+++ CoreMapper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
+using GordonApplepie.NetCoreMapper.Annotations;
 
 namespace GordonApplepie.NetCoreMapper
 {
@@ -14,10 +15,28 @@ namespace GordonApplepie.NetCoreMapper
 
             foreach (PropertyInfo p in obj.GetType().GetProperties())
             {
-                var property = properties.Find(x => x.Name.ToLower() == p.Name.ToLower());
-                if (property != null)
-                    property.SetValue(variable, p.GetValue(obj));
+                var nomap = p.GetCustomAttribute<NoMapAttribute>();
+                var onlymap = p.GetCustomAttribute<OnlyMapAttribute>();
+                var mapon = p.GetCustomAttribute<MapOnAttribute>();
 
+                if ((nomap == null || nomap.DoMap(typeof(T))) && 
+                    (onlymap == null || onlymap.DoMap(typeof(T)))) {
+
+                    // map on same property name
+                    if (mapon == null || mapon.AllowedProperties.Count == 0)
+                    {
+                        var property = properties.Find(x => x.Name.ToLower() == p.Name.ToLower());
+                        if (property != null)
+                            property.SetValue(variable, p.GetValue(obj));
+                    }
+                    // map all properties with selected name
+                    else
+                    {
+                        var list = properties.FindAll(x => mapon.AllowedProperties.Contains(x.Name.ToLower()));
+                        for (var i = 0; i < list.Count; i++)
+                            list[i].SetValue(variable, p.GetValue(obj));
+                    }
+                }
             }
 
             return (T)variable;
@@ -29,10 +48,28 @@ namespace GordonApplepie.NetCoreMapper
 
             foreach (PropertyInfo p in obj.GetType().GetProperties())
             {
-                var property = properties.Find(x => x.Name.ToLower() == p.Name.ToLower());
-                if (property != null)
-                    property.SetValue(objToMap, p.GetValue(obj));
+                var nomap = p.GetCustomAttribute<NoMapAttribute>();
+                var onlymap = p.GetCustomAttribute<OnlyMapAttribute>();
+                var mapon = p.GetCustomAttribute<MapOnAttribute>();
 
+                if ((nomap == null || nomap.DoMap(objToMap.GetType())) &&
+                    (onlymap == null || onlymap.DoMap(objToMap.GetType()))) {
+
+                    // map on same property name
+                    if (mapon == null || mapon.AllowedProperties.Count == 0)
+                    {
+                        var property = properties.Find(x => x.Name.ToLower() == p.Name.ToLower());
+                        if (property != null)
+                            property.SetValue(objToMap, p.GetValue(obj));
+                    }
+                    // map all properties with selected name
+                    else
+                    {
+                        var list = properties.FindAll(x => mapon.AllowedProperties.Contains(x.Name.ToLower()));
+                        for (var i = 0; i < list.Count; i++)
+                            list[i].SetValue(objToMap, p.GetValue(obj));
+                    }
+                }
             }
         }
     }
